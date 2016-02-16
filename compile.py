a39259f017efa5d8e086fd878aff2c5be8a1f917
root <root@ip-172-31-41-140.us-west-2.compute.internal>
2016-02-16 17:42:52 +0000

    fixed untracked files

--- compile.py
+++
@@ -1,7 +0,0 @@
-import py_compile
-
-py_compile.compile("dagah.py")
-py_compile.compile("modempoller.py")
-py_compile.compile("apipoller.py")
-py_compile.compile("agentpoll.py")
-py_compile.compile("agentattachpoller.py")
